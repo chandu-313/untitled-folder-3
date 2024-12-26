73237e08036222f2c39a3e835b2bbf42508d02e0
chandu-313 <chanduneeraj313@gmail.com>
2024-12-26 21:27:27 +0530

    m

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+a={"car":2007,"Honda":8181,"India":10}
+print(a)
+print(len(a))
+print(type(a))
+print(a.keys())
+print(a.values())
+a["Canada"]=40
+print(a)
+a.pop("Honda")
+print(a)
---
+++ listtodict.py
@@ -0,0 +1,7 @@
+a=["name","grade","age"]
+b=["chandu",12,17]
+c={}
+c[a[0]]=b[0]
+c[a[1]]=b[1]
+c[a[2]]=b[2]
+print(c)
